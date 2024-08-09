5881b008bb117401bdb65313421ddb65fcf32614
dannyrichf1 <138616000+dannyrichf1@users.noreply.github.com>
2024-08-09 17:43:50 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-var imgDiv = document.getElementById("mvp-post-feat-img");
-var postContent = document.getElementById("mvp-post-content");
-var getpostImg = postContent.getElementsByTagName("img");
-var postImg = '<img style="width:100%;" src="'+getpostImg[0].src+'"/>';
-imgDiv.innerHTML = postImg;
