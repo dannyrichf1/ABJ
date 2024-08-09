6c992c541796ce10f59839822f8fdd9b097a0dab
dannyrichf1 <138616000+dannyrichf1@users.noreply.github.com>
2024-08-09 17:44:19 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+var imgDiv = document.getElementById("mvp-post-feat-img");
+var postContent = document.getElementById("mvp-post-content");
+var getpostImg = postContent.getElementsByTagName("img");
+var postImg = '<img style="width:100%;" src="'+getpostImg[0].src+'"/>';
+imgDiv.innerHTML = postImg;
