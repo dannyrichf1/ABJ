2e81ce05fc03b01e1888bbdefcf9a397218c2ecf
dannyrichf1 <138616000+dannyrichf1@users.noreply.github.com>
2024-08-09 17:22:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var imgDiv = document.getElementById("mvp-post-feat-img");
 var getpostImg = document.getElementsByTagName("img");
-console.log(getpostImg[2].src);
-var postImg = '<img src="'+getpostImg[0].src+'"/>';
+var postImg = '<img style="width:100%;" src="'+getpostImg[0].src+'"/>';
 imgDiv.innerHTML = postImg;
